2a40f7523d497f215d2447f47e842498f4d88590
devxiaofan <1353815633@qq.com>
2023-06-20 11:54:52 +0800

    lazyCompoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, {useState, lazy, Suspense} from 'react';
 import largeImg from '@/assets/imgs/large.png';
 import smallImg from '@/assets/imgs/small.png';
-import { Demo1, Demo2 } from '@/components';
+import {Demo1, Demo2} from '@/components';
 import '@/app.css';
 import '@/app.less';
 
+const LazyDemo = lazy(() => import('@/components/LazyDemo')); // 懒加载
+
 function App() {
     const [count, setCount] = useState(0);
+    const [show, setShow] = useState(false);
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setCount(Number(e.target.value));
     }
 
+    const onClick = () => {
+        import('./app.css')
+        setShow(!show);
+    }
+
     return (
         <>
-            <div className='smallImg'></div>
-            <div className='largeImg'></div>
+            <h2 onClick={onClick}>on Show</h2>
+            {show && <Suspense fallback={<div>loading...</div>}> <LazyDemo/>
+            </Suspense>}
         </>
     )
 }
---
+++ src/components/LazyDemo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function LazyDemo() {
+    return (
+        <div>
+            <h3>LazyDemo</h3>
+        </div>
+    )
+}
+export default LazyDemo;
